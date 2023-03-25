f6e48435f77e98a499416defb41068e7bcffdd4f
Nazar Windows 10 <you@example.com>
2023-03-25 16:59:41 +0300

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,32 +9,23 @@ const reorderColumnList = (sourceCol, startIndex, endIndex) => {
     const newTaskIds = Array.from(sourceCol.taskIds)
     const [removed] = newTaskIds.splice(startIndex, 1)
     newTaskIds.splice(endIndex, 0, removed)
-
     const newColumn = {
         ...sourceCol,
         taskIds: newTaskIds,
     }
-
     return newColumn;
 }
-
 
 const App = () => {
     const [state, setState] = React.useState(initialData)
     const onDragEnd = ( result ) => {
         const { destination, source } = result;
-
-
         //IF TRY TO DROP IN AN UNKNOWN destination
     if(!destination) return;
-
-
     //IF THE DRAGS AND DROPS BACK IN THE SAME PLACE
     if(destination.probbableId === source.droppableId && destination.index === source.index){
         return
     }
-
-
     //IF THE USER DROPS WITH IN SAME COLUMN BUT IN A DIFFERENT POSITION
     const sourceCol = state.columns[source.droppableId]
     const destinationCol = state.columns[destination.droppableId]
@@ -44,7 +35,6 @@ const App = () => {
             source.index,
             destination.index
         )
-
         const newState = {
             ...state,
             columns: {
@@ -64,7 +54,6 @@ const App = () => {
         ...sourceCol,
         taskIds: startTaskIds,
     }
-
     const  endTaskIds = Array.from(destinationCol.taskIds)
     endTaskIds.splice(destination.index, 0, removed)
     const newEndCol = {
@@ -80,8 +69,8 @@ const App = () => {
         }
     }
     setState(newState)
-
     }
+
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
@@ -90,7 +79,7 @@ const App = () => {
             {state.columnOrder.map((columnId) => {
                 const column = state.columns[columnId];
                 const tasks = column.taskIds.map(taskId => state.tasks[taskId])
-                return <Column key={column.id} column={column} tasks={tasks} />
+                return <Column key={column.id} state={state} setState={setState} column={column} tasks={tasks} />
             })}
         </div>
     </div>
@@ -103,19 +92,17 @@ export default App
 
 
 
-const initialData = {
+export const initialData = {
     tasks: {
         1: { id: 1, content: "Configure Next.js application" },
         2: { id: 2, content: "Configure Next.js and tailwind" },
         3: { id: 3, content: "Create slidebar navigation menu" },
-        4: { id: 4, content: "Create page footer" },
-        5: { id: 5, content: "Create page navigationmenu" },
     },
     columns: {
         "column-1": {
             name: 'To do',
             id: "column-1",
-            taskIds: [1,2,3,4,5]
+            taskIds: [1,2,3/* ,4,5 */]
         },
         "column-2": {
             name: 'Doing',
--- src/components/Column/Column.jsx
+++ src/components/Column/Column.jsx
@@ -1,27 +1,163 @@
-import React from "react";
+import React, { createRef } from "react";
 import { Draggable, Droppable } from "react-beautiful-dnd";
+import { initialData } from "../App/App";
 import Card from "../Card/Card";
 
-const Column = ({ column, tasks }) => {
+const Column = ({ column, tasks, state, setState }) => {
+  var buttonRef = createRef(null);
+  const [inputHide, setInputHide] = React.useState(false);
+  //IN ASAKY INPUT
+  const [inputText, setInputText] = React.useState("");
+  const [editHide, setEditHide] = React.useState(0);
+  const [editText, setEditText] = React.useState("");
+  const [newItemIndex, setNewItemIndex] = React.useState(4)
+
+  //ON CLICK OPEN BUTTON OPENS INPUT WITH FOCUS
+  React.useEffect(() => {
+    if (buttonRef.current) {
+      buttonRef.current.focus();
+    }
+  }, [buttonRef]);
+  //ON CLICK OPEN BUTTON OPENS INPUT WITH FOCUS
+
+  const onClickCloseButton = () => {
+    setInputHide(!inputHide);
+    setInputText("");
+  };
+
+  const onClickOpenButton = () => {
+    setInputHide(!inputHide);
+  };
+
+  const onChangeInput = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const onClickAddButton = () => {
+    if (inputText === "") {
+      return
+    } else {
+      setInputHide(!inputHide);
+      //ADDING NEW TASK
+      const newItem = () => {
+      }
+      setState(
+        ...state,
+      )
+    }
+  };
+
+  const onCLickEdit = (id, index) => {
+    setEditHide(prev => !prev)
+    setEditHide(index + 1)
+    console.log(id.content)
+    setEditText(id.content)
+  };
+
+  const onCLickEditClose = (id) => {
+    setEditHide(0)
+    setEditText('')
+  };
+
+  const onClickEditSave = (id, index) => {
+    setState({
+      ...state,
+      ...(id.content = editText),
+    });
+    setEditHide(prev => !prev)
+    setEditText('')
+  }
+
+  const onClickDelete = (id, index) => {
+    // console.log(index)
+    
+    const newTask = tasks.slice(index + 1)
+    const newTaskIds = column.taskIds.slice(index + 1)
+    console.log(column.taskIds.splice(index, 1))
+    setState(
+      {
+        ...state,
+        // tasks: {
+        //   ...state.tasks,
+        //   [index + 1]: newTask
+        // },
+      }
+    )
+  };
 
   return (
     <Droppable droppableId={column.id}>
       {(droppableProvided, droppableSnapshot) => (
-        <div className="col" ref={droppableProvided.innerRef} {...droppableProvided.droppableProps}>
+        <div
+          className="col"
+          ref={droppableProvided.innerRef}
+          {...droppableProvided.droppableProps}
+        >
           <h2>{column.name}</h2>
           {tasks.map((task, index) => {
             return (
-                <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
-                    {( draggableProvided, droppableSnapshot ) => (
-                        <div className="colInnerActive"
-                            ref={draggableProvided.innerRef} {...draggableProvided.draggableProps} {...draggableProvided.dragHandleProps}>
-                            <h2>{task.content}</h2>
-                            <div className="leftSideBorder"></div>
+              <Draggable key={task.id} draggableId={`${task.id}`} index={index}>
+                {(draggableProvided, droppableSnapshot) => (
+                  <div
+                    className="colInnerActive"
+                    ref={draggableProvided.innerRef}
+                    {...draggableProvided.draggableProps}
+                    {...draggableProvided.dragHandleProps}
+                  >
+                    <h2>{task.content}</h2>
+                    <div className="leftSideBorder"></div>
+                    {editHide == index + 1 ? (
+                      <div>
+                        <textarea onChange={(e) => setEditText(e.target.value)} type="text" value={editText} placeholder="EDIT" className="editTextarea"></textarea>
+                        <div className="row2">
+                        <button onClick={() => onClickEditSave(task, index)}>EDIT SAVE</button>
+                        <button onClick={onCLickEditClose}>X</button>
                         </div>
+                      </div>
+                    ) : (
+                      <div>
+                        <button onClick={() => onCLickEdit(task, index)}>EDIT</button>
+                        <button onClick={() => onClickDelete(task, index)}>
+                          Delete
+                        </button>
+                      </div>
                     )}
-                </Draggable>
-            )
+                  </div>
+                )}
+              </Draggable>
+            );
           })}
+          {inputHide == false ? (
+            <button onClick={onClickOpenButton} className="taskAddButton">
+              Добавить карточку
+            </button>
+          ) : (
+            ""
+          )}
+          {inputHide && (
+            <div className="inputType">
+              <textarea
+                ref={buttonRef}
+                value={inputText}
+                onChange={(e) => onChangeInput(e)}
+                placeholder="Ввести заголовок для этой карточки"
+              ></textarea>
+              <div className="row2">
+                <button
+                  onClick={onClickAddButton}
+                  className="taskAddInputButton"
+                >
+                  Добавить карточку
+                </button>
+                <button
+                  onClick={onClickCloseButton}
+                  className="closeInputButton"
+                >
+                  X
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       )}
     </Droppable>
